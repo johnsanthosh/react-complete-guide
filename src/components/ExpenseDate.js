b7b4bdcbba5247e4fb5b2a60fb74be4cc17aebdb
John Santhosh <johnsanthosh@users.noreply.github.com>
2022-07-18 18:21:27 +0530

    some more changes

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+
+const ExpenseDate = ({ date }) => {
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
+  const year = date.getFullYear();
+
+  return (
+    <div>
+      <div>{month}</div>
+      <div>{day}</div>
+      <div>{year}</div>
+    </div>
+  );
+};
+
+export default ExpenseDate;
+
+ExpenseDate.propTypes = {
+  date: PropTypes.object,
+};
