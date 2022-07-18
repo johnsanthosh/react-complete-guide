aafb8404222fd2ff74f6ae4a4c6666b69833da56
John Santhosh <john@Johns-MacBook-Pro.local>
2022-07-18 17:57:47 +0530

    Improve expense item.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,30 @@
+import PropTypes from "prop-types";
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
+const ExpenseItem = ({ title, amount, date }) => {
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
+  const year = date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>Jul 18, 2022</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$293</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
 };
 
 export default ExpenseItem;
+
+ExpenseItem.protoTypes = {
+  title: PropTypes.string,
+  amount: PropTypes.number,
+  date: PropTypes.date,
+};
