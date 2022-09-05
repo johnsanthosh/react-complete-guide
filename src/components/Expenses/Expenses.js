214464b9c6644573925f8d16a7d2b7aaa6f89854
John Santhosh <johnsanthosh@users.noreply.github.com>
2022-09-05 21:49:30 +0530

    Filter expenses by year.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,7 +5,7 @@ import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 const Expenses = ({ expenses }) => {
-const [filteredYear, setFilteredYear] = useState('2020'); 
+  const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
@@ -14,10 +14,15 @@ const [filteredYear, setFilteredYear] = useState('2020');
   return (
     <>
       <Card className="expenses">
-        <ExpensesFilter selectedValue={filteredYear} onChange={filterChangeHandler} />
-        {expenses.map((expense) => (
-          <ExpenseItem key={expense.id} {...expense} />
-        ))}
+        <ExpensesFilter
+          selectedValue={filteredYear}
+          onChange={filterChangeHandler}
+        />
+        {expenses
+          .filter((expense) => expense.date.getFullYear().toString() === filteredYear)
+          .map((expense) => (
+            <ExpenseItem key={expense.id} {...expense} />
+          ))}
       </Card>
     </>
   );
